c25c56f859794beba903e6ef92c6aa56ba3f5460
MilesKurts <milesheremau@gmail.com>
2024-01-05 22:28:20 -0300

    Ex 4

---
+++ ex_4.py
@@ -0,0 +1,6 @@
+n1 = input('insert a value: \n')
+print('The primitive tipe is ', type(n1))
+print(n1.isspace(), '\n')
+print(n1.isnumeric(), '\n')
+print(n1.isalpha(), '\n')
+print(n1.isalnum(), '\n')
