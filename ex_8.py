2f2e9e5781889f44e2d75d8b42c196914d83834a
MilesKurts <milesheremau@gmail.com>
2024-01-07 09:28:44 -0300

    ex9

--- ex_8.py
+++ ex_8.py
@@ -1,4 +1,4 @@
 met = float(input('Insert a meter value:\n'))
 c = met *100
 m = met * 1000
-print('the value of {} in milimiters is {} and centimeters is {}!'.format(met, m, c))
+print('the value of {}m in milimiters is {} and centimeters is {}!'.format(met, m, c))
---
+++ ex_9.py
@@ -0,0 +1,10 @@
+n1 = int(input('insert a number:\n'))
+i = 0
+
+while i < 10:
+    i += 1
+    r = i * n1
+    print('{}  x  {} = {}'.format(n1, i, r))
+    
+    
+    
