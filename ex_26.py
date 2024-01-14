5e27d2991a15345d094a87aac737d01bd2a62455
MilesKurts <milesheremau@gmail.com>
2024-01-13 21:26:57 -0300

    ex_26

--- ex_26.py
+++ ex_26.py
@@ -1,4 +1,4 @@
-frase = str(input('Insert a sentence: \n')).upper()
+frase = str(input('Insert a sentence: \n')).upper().strip()
 print('A letra A aparece {} vezes na frase!'.format(frase.count('A')))
 print('A primeira letra a esta {}'.format(frase.find('A')+1))
 print('A ultima letra a esta {}'.format(frase.rfind('A')+1))
