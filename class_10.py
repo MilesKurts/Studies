428c69304a0d92041b521bb62b921c90860f6373
MilesKurts <milesheremau@gmail.com>
2024-01-16 23:04:22 -0300

    class 10

---
+++ class_10.py
@@ -0,0 +1,14 @@
+#nome = str(input('Whats your name: '))
+#if nome == 'Miles Kurts':
+#    print("What a beautyfull name!")
+#else:
+#    print("Strange name")
+#print("Good morning, {}".format(nome))
+n1 = float(input("A value: "))
+n2 = float(input("another value: "))
+m = (n1 + n2)/2
+print("Your media is {}".format(m))
+if m >= 6.0:
+    print("GOOD ONE!")
+else:
+    print("Get better")
