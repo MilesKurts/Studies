3b19c58b998c8819f4d09e5c8c8838952f3d086a
MilesKurts <milesheremau@gmail.com>
2024-01-17 00:22:28 -0300

    ex 28

--- ex_28.py
+++ ex_28.py
@@ -1,9 +1,19 @@
-import random
-n = random.choice([0,5])
-c = int(input("CHOOSE A NUMBER BETWEEN 0 AND 5: "))
-if c == n:
-    print("Congratulation!")
-elif c >= 6:
-    print("ERROR")
-elif c != n:
-    print("Wrong!")
+#import random
+#n = random.choice([0,5])
+#c = int(input("CHOOSE A NUMBER BETWEEN 0 AND 5: "))
+#if c == n:
+#    print("Congratulation!")
+#elif c >= 6:
+#    print("ERROR")
+#elif c != n:
+#    print("Wrong!")
+
+from random import randint
+comp =randint(0,5)
+print('-=-'*20)
+print('Thinking about a number... try me: ')
+jog = int(input("What one i choose? "))
+if jog == comp:
+    print('perfect')
+else:
+    print("wrong!")
