4f0b001ae4bac8fc802b2860f2e47b5a34048fd8
MilesKurts <milesheremau@gmail.com>
2024-01-20 23:31:02 -0300

    ex 36

--- ex_36.py
+++ ex_36.py
@@ -2,5 +2,10 @@ house = float(input("How much is the house? "))
 salary = float(input("What's your salary? "))
 years_paying = int(input("How much years are you paying?"))
 times = house/(years_paying*12)
+min = salary * 30/100
 print("Para pagar uma casa de R${:.2f} em {} anos".format(house,years_paying) )
 print(" a prestação será de R${:.2f}".format(times))
+if times <= min:
+    print("OK")
+else:
+    print("NOT OK")
