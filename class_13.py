e2bdf2fb5d5636ec662b389cb30b9d18095fe7c1
MilesKurts <milesheremau@gmail.com>
2024-01-30 23:16:08 -0300

    class 13

---
+++ class_13.py
@@ -0,0 +1,18 @@
+#for c in range(1,11):
+#     print(c)
+#print("fim")
+
+
+#i = int(input("inicio: "))
+#f = int(input("fim: "))
+#p = int(input("passo: "))
+#for c in range(i, f+1, p):
+#    print(c)
+#print("Fim")
+
+
+s = 0
+for c in range(0,4):
+    n = int(input("Digite um valor: "))
+    s += n
+print(s)
