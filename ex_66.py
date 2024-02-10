fb4ee923dca255deda3ecca9657694c099f3b317
MilesKurts <milesheremau@gmail.com>
2024-02-09 21:18:03 -0300

    ex 66

---
+++ ex_66.py
@@ -0,0 +1,8 @@
+i = s =0
+while True:
+    n = int(input("Type a number: (999 to stop)"))
+    if n == 999:
+        break
+    s += n
+    i += 1
+print(f"There was typed {i} number, and their sum are {s}")
