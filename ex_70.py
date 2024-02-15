e20f58d018d6f683cad95f80d498d42c47c37f09
MilesKurts <milesheremau@gmail.com>
2024-02-15 11:44:52 -0300

    ex 70

--- ex_70.py
+++ ex_70.py
@@ -6,18 +6,17 @@ barato = " "
 while True:
     produto = str(input("Nome do produto: "))
     preço = float(input("Qual o preço?"))
-    
+    cont += 1
     total += preço
     if preço > 1000:
         totmil += 1
     if cont == 1 or preço < menor:
         menor = preço
         barato = produto
-    cont += 1
     resp = " "
     while resp not in "SN":
         resp = str(input("Quer continuar? [S/N]")).strip().upper()[0]
     if resp == "N":
         break
 print("FIM!")
-print(f"sua compra ficou {total} e {totmil} produto(s) foram mais de 1000 reais e o produto mais barato custa {menor} e foi a(o) {produto}!")
+print(f"sua compra ficou {total} e {totmil} produto(s) foram mais de 1000 reais e o produto mais barato custa {menor} e foi a(o) {barato}!")
