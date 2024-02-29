6c9bf6406f79d7870ec127faf9f07ed084ed12e2
MilesKurts <milesheremau@gmail.com>
2024-02-29 12:49:13 -0300

    class 17

---
+++ class_17.py
@@ -0,0 +1,19 @@
+#num = [2,5,9,1]
+#num.append(7)
+#num.sort(reverse=True)
+#num.insert(2,2)
+#num.pop(2)
+#if 3 in num:
+#    num.remove(4)
+#else:
+#    print("Não achei o numero 4")
+#print(num)
+#print(f'essa lista tem {len(num)} elementos')
+valores = []
+valores.append(5)
+valores.append(9)
+valores.append(4)
+
+for c, v in enumerate(valores):
+    print(f"na posição {c} encontrei {v}")
+print("Cheguei no final")
