4c604c040a60819fc0a7d7511d77a32e1c2f671c
MilesKurts <milesheremau@gmail.com>
2024-03-26 12:35:37 -0300

    add class

---
+++ M01A03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A03
+{
+    internal class Program
+    {
+        /// <summary>
+        /// metodo exec automaticamente 
+        /// ou pela linha de comando
+        /// </summary>
+        /// <param name="args">vetor que pode conter paramentros de execução
+        /// </param>
+        /// <code>
+        /// podemos exec como program.exe...
+        /// </code>
+        static void Main(string[] args)
+        {
+
+
+            //Hellooo!!
+
+
+            /*hey bro
+             * 
+             * 
+             * 
+             */
+            Console.WriteLine("Comments");
+        }
+    }
+}
