dffab3c71e67d40a21e914dcd219cc1c24d20934
MilesKurts <milesheremau@gmail.com>
2024-03-28 13:06:00 -0300

    class 5

---
+++ M01A05/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nome = "miles";
+
+            Console.WriteLine("A variavel n tem "+nome);
+            Console.WriteLine("N é do tipo " + nome.GetType());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ M01A05B/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A05B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte idade = 25;
+            short publico = 25239;
+            float media = 4.5f;
+            decimal estrela = 4.9847373723m;
+            Console.WriteLine(estrela);
+            Console.ReadKey();
+        }
+    }
+}
