08f0c9dbef431db0f1622ba8f5b719976ec195c3
MilesKurts <milesheremau@gmail.com>
2024-03-28 10:52:28 -0300

    commit

--- 1site.py/routes/cliente.py
+++ 1site.py/routes/cliente.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
+from database.cliente import CLIENTES
 
-cliente_route = Blueprint("cliente ", __name__)
+cliente_route = Blueprint("cliente", __name__)
 
 '''
 Rota de clientes
@@ -18,27 +19,40 @@ Rota de clientes
 
 @cliente_route.route("/")
 def lista_clientes():
-    return {"pagina": 'lista_clientes'}
+    '''Listar clientes'''
+    return render_template('lista_clientes.html', clientes=CLIENTES)
 
-@cliente_route.route("/")
+@cliente_route.route("/", methods=['POST'])
 def inserir_cliente():
     '''Iserir dados do cliente no banco de dados'''
-    pass
+
+    data = request.json
+    
+    novo_usuario = {
+        'id': len(CLIENTES)+1,
+        'nome': data['nome'],
+        'email': data['email'],
+    }
+
+    CLIENTES.append(novo_usuario)
+
+    return render_template('item_cliente.html', cliente='novo_usuario')
+    
 
 @cliente_route.route("/new")
 def form_cliente():     
     ''' Formulario para cadastro de cliente'''
-    return {'pagina': 'formulario clientes'}
+    return render_template('form_cliente.html')
 
 @cliente_route.route("/<int:cliente_id>")
 def detalhe_cliente(cliente_id):
     '''Exibir detalhes do cliente'''
-    pass
+    return render_template('detalhe_cliente.html')
 
 @cliente_route.route("/<int:cliente_id>/edit")
 def form_edit_cliente(cliente_id):
     '''Formulario para editar um cliente'''
-    pass
+    return render_template('form_edit_cliente.html')
     
 @cliente_route.route("/<int:cliente_id>/update", methods=['PUT'])
 def atualizar_cliente(cliente_id):
