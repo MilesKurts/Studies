1829c6c657dcad0ae9fc8cafcac8a699411b44d4
MilesKurts <milesheremau@gmail.com>
2024-03-28 01:01:17 -0300

    update

--- 1site.py/main.py
+++ 1site.py/main.py
@@ -1,23 +1,10 @@
-from flask import Flask, url_for, render_template
-
+from flask import Flask
+from routes.home import home_route
 #beggin flask
 app = Flask(__name__)
 
  #routs
-@app.route("/")
-def ola_mundo():
-    titulo = "Gestão usuarios"
-    usuarios = [
-        {"nome": "Guilherme", "membro_ativo": True},
-        {"nome": "joao", "membro_ativo": False},
-        {"nome": "Maria", "membro_ativo": False},
-    ]
-
-    return render_template('index.html', titulo=titulo,usuarios=usuarios)
-
-@app.route("/sobre")
-def pagina_sobre():
-    return "Eae bro!"
+app.register_blueprint(home_route)
 
 #final flask
 app.run(debug=True)
