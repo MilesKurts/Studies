d46a768a7346d51b07092b277d29a37162356873
MilesKurts <milesheremau@gmail.com>
2024-03-31 23:58:29 -0300

    new

---
+++ M01A06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string escola = "Estudonauta"; 
+
+            Console.WriteLine("Eu estudo no "+escola);
+            Console.WriteLine("O valor de pi é "+ Math.PI);
+            Console.ReadKey();
+        }
+    }
+}
