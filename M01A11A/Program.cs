eef46f3e29fd79b52330c31932091799df1766c5
MilesKurts <milesheremau@gmail.com>
2024-04-01 08:13:35 -0300

    THREAD.SLEEP

---
+++ M01A11A/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace M01A11A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia = DateTime.Now.Day;
+            int mes = DateTime.Now.Month;
+            int ano = DateTime.Now.Year;
+            Console.Write("hoje é o dia "+dia); 
+            Thread.Sleep(4000);
+            Console.Write(" Do mês " + mes); 
+            Thread.Sleep(4000);
+            Console.Write(" Do ano "+ano);
+            Thread.Sleep(4000);
+            Console.Write(" Thanks");
+            Thread.Sleep(1200);
+        }
+    }
+}
