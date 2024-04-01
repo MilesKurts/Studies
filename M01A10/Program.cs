451778a8bd258f043e0228d85668b169d00c64cf
MilesKurts <milesheremau@gmail.com>
2024-04-01 07:50:31 -0300

    DateTime

---
+++ M01A10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dia = DateTime.Now.Day;
+            int mes = DateTime.Now.Month;
+            int ano = DateTime.Now.Year;
+            Console.WriteLine("A data atual é dia "+dia+" do mês "+mes+" do ano "+ano);
+
+            int hora = DateTime.Now.Hour;
+            int min = DateTime.Now.Minute;
+            int sec = DateTime.Now.Second;
+            Console.WriteLine("A hora atual é " + hora + " horas " + min + " minuto(s) " + sec+" segundo(s)");
+            Console.ReadKey();
+        }
+    }
+}
