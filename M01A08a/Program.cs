8770f4c52ba9cb787b711ea9c8c7e809de7ba3e8
MilesKurts <milesheremau@gmail.com>
2024-04-01 00:32:36 -0300

    typing values

---
+++ M01A08a/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A08a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Muito prazer em te conhecer "+nome);
+            Console.ReadKey();
+        }
+    }
+}
