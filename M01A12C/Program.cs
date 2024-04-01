917661140df508540aefb1efd68e572e507f10d0
MilesKurts <milesheremau@gmail.com>
2024-04-01 09:07:20 -0300

    new

---
+++ M01A12C/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A12C
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float sal = 0;
+            Console.WriteLine("qual é o seu salário? ");
+            float.TryParse(Console.ReadLine(), out sal);
+            Console.WriteLine($"Você ganha {sal:C}");
+            Console.ReadKey();
+        }
+    }
+}
