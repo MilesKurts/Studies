9390158b9075abf1e67d9585ea8c0796903f99cf
MilesKurts <milesheremau@gmail.com>
2024-04-01 08:25:55 -0300

    delay

---
+++ M01A11b/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M01A11b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            lblmsg.Text = "Hello, how are you? ";
+            await Task.Delay(2000);
+            lblmsg.Text += "what are you doing?";
+        }
+    }
+}
