36d27b1582854da354441a39fdd890093f7b1dd0
MilesKurts <milesheremau@gmail.com>
2024-04-01 23:35:02 -0300

    let's go

---
+++ M01A13A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01A13A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "Gustavo";
+            float sal = 555.7556f;
+            int idade = 22;
+            Console.WriteLine($"{nome, 10} tem {idade, -5:D3} Ganha {sal,20:C3} por mes");
+            Console.ReadKey();
+        }
+    }
+}
