f78e09f00cfd86038fdf40d2a68f232e4a2fd328
MilesKurts <milesheremau@gmail.com>
2024-04-02 09:37:04 -0300

    ex1

---
+++ M01E01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace M01E01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("MEU ");
+            Console.ResetColor();
+            
+            Console.BackgroundColor= ConsoleColor.Yellow;
+            Console.ForegroundColor= ConsoleColor.Green;
+            Thread.Sleep(1000);
+            Console.Write("BRASIL ");
+            Console.ResetColor();
+
+            Thread.Sleep(1000);
+            Console.Write("BRASILEIRO!");
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+    }
+}
