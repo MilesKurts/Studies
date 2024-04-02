e9dfb8a1d91261895264daedda9c21e44570fc2d
MilesKurts <milesheremau@gmail.com>
2024-04-02 10:02:22 -0300

    ex02

--- M01E01/Program.cs
+++ M01E01/Program.cs
@@ -11,20 +11,23 @@ namespace M01E01
     {
         static void Main(string[] args)
         {
+            Console.SetCursorPosition(10, 4);
             Thread.Sleep(1000);
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Write("MEU ");
+            Console.Write(" MEU ");
             Console.ResetColor();
             
             Console.BackgroundColor= ConsoleColor.Yellow;
             Console.ForegroundColor= ConsoleColor.Green;
             Thread.Sleep(1000);
-            Console.Write("BRASIL ");
+            Console.Write(" BRASIL ");
             Console.ResetColor();
 
             Thread.Sleep(1000);
-            Console.Write("BRASILEIRO!");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine(" BRASILEIRO!");
             Console.ReadKey();
             Console.ResetColor();
         }
