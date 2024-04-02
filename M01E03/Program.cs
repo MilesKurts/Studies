2e62270038006ee49248bc683b3c03bf155ae852
MilesKurts <milesheremau@gmail.com>
2024-04-02 10:22:25 -0300

    ex03

---
+++ M01E03/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01E03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero Real");
+            float num;
+            float.TryParse(Console.ReadLine(), out num);
+
+            int n1 = (int) num;
+            int n2 = Convert.ToInt16(num);
+
+            Console.WriteLine($"Voce digitou o numero {num:N}");
+            Console.WriteLine($"A parte inteira de num é {n1}");
+            Console.WriteLine($"Arredondando fica {n2}");
+
+            Console.ReadKey();
+        }
+    }
+}
