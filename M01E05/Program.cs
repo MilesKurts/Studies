62e8015f8a6890baed651cc7a425afbf9cb6eff4
MilesKurts <milesheremau@gmail.com>
2024-04-02 10:50:25 -0300

    ex05

---
+++ M01E05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M01E05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que ano voce nasceu?");
+            int nasc;
+            int.TryParse(Console.ReadLine(), out nasc);
+            int atual = DateTime.Now.Year;
+            int idade = atual - nasc;
+            Console.WriteLine($"Voce tem {idade} anos de idade");
+            Console.ReadKey();
+
+        }
+    }
+}
