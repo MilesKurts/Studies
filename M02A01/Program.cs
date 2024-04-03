f6d2a3707168ceb07e990576e2ae72fb0b5c731b
MilesKurts <milesheremau@gmail.com>
2024-04-03 12:21:18 -0300

    Module 2

---
+++ M02A01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02A01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //EXEMPLO DE OP ARITMÉTICOS
+            int op1, op2 = 0;
+
+            //entrada de dados//
+            Console.WriteLine("Digite o primeiro numero: ");
+            int.TryParse(Console.ReadLine(), out op1);
+
+            Console.WriteLine("Digite o Segundo numero: ");
+            int.TryParse(Console.ReadLine(), out op2);
+
+            //calculos//
+            Console.WriteLine($"Calculando +{op1} = {+ op1}");
+            Console.WriteLine($"Calculando -{op2} = {-op2}");
+            Console.WriteLine($"Calculando {op1} + {op2} = {op1+ op2}");
+            Console.WriteLine($"Calculando {op1}-{op2}={op1-op2}");
+            Console.WriteLine($"Calculando {op1}x{op2}={op1 * op2}");
+            Console.WriteLine($"Calculando {op1}/{op2}={op1/op2}/divisão inteira");
+            Console.WriteLine($"Calculando {op1} % {op2}={op1%op2} /resto da divisão inteira");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ test01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 156;
+            Console.WriteLine(n1.GetType());
+            Console.ReadKey();
+        }
+    }
+}
