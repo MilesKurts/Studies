7b8a7fc7e88a9e91d7365d4a0c2c170907bf77af
MilesKurts <milesheremau@gmail.com>
2024-04-04 10:30:01 -0300

    M01A07

---
+++ M02A07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02A07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 0;
+            Console.WriteLine("digite um numero: ");
+            int.TryParse(Console.ReadLine(), out n1);
+            Console.WriteLine("Digite outro valor: ");
+            int.TryParse(Console.ReadLine(), out n2);
+            Console.WriteLine($"{n1} = {n2}? {n1==n2}");
+            Console.WriteLine($"{n1} != {n2}={n1!=n2}");
+            Console.WriteLine($"{n1} > {n2}={n1 > n2}");
+            Console.WriteLine($"{n1} < {n2}={n1 < n2}");
+            Console.WriteLine($"{n1} >= {n2}={n1 >= n2}");
+            Console.WriteLine($"{n1} <= {n2}={n1 <= n2}");
+            Console.ReadKey();
+        }
+    }
+}
