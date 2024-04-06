a4981b22564eadd26afb8fb9c8069d586915aead
MilesKurts <milesheremau@gmail.com>
2024-04-05 23:21:14 -0300

    M02A09

---
+++ M02A09/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02A09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool p = true;
+            bool q = false;
+
+            Console.WriteLine($"não(~) {p}");
+            Console.WriteLine($"{p} e {q} = {p && q}");
+            Console.WriteLine($"{p} ou {q} = {p || q}");
+            Console.ReadKey();
+        }
+    }
+}
