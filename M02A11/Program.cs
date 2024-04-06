e9c93b02e0883a0674e399842befdd22a601f650
MilesKurts <milesheremau@gmail.com>
2024-04-06 20:01:51 -0300

    M02A11

---
+++ M02A11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02A11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float altura = 0f;
+            Console.WriteLine("Qual sua altura? ");
+            float.TryParse(Console.ReadLine(), out altura);
+            string status = (altura <= 1.70)?"baixa":"alta";
+            Console.WriteLine($"{status}");
+            Console.ReadKey(); 
+        }
+    }
+}
