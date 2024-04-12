838043a30495729201665dcc7d8cbc87be41289a
MilesKurts <milesheremau@gmail.com>
2024-04-11 23:31:41 -0300

    new

---
+++ M02A15/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02A15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(26 & 12);
+            Console.WriteLine(26 | 12);
+            Console.WriteLine(26 ^ 12);
+            Console.ReadKey();
+        }
+    }
+}
