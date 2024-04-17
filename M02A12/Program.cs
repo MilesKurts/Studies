8239f9e59f1481c8f61cf886ed950d2d6bf5701d
MilesKurts <milesheremau@gmail.com>
2024-04-17 19:56:25 -0300

    new

--- M02A12/Program.cs
+++ M02A12/Program.cs
@@ -17,6 +17,7 @@ namespace M02A12
             Console.WriteLine($"O numero {n} corresponde a {Convert.ToString(n, toBase: 2)} em binario");
             Console.WriteLine($"O numero {n} corresponde a {Convert.ToString(n, toBase: 8)} em binario");
             Console.WriteLine($"O numero {n} corresponde a {Convert.ToString(n, toBase: 16)} em binario");
+            Console.WriteLine($"O numero {n} corresponde a {Convert.ToString(n, toBase: 16)} em binario");
 
             Console.ReadLine();
         }
