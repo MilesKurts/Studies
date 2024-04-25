b2775dca4f0f8cbd95c399040b608762b9cce4ee
MilesKurts <milesheremau@gmail.com>
2024-04-24 22:44:00 -0300

    m02ex11

---
+++ M02Ex11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int op1 = 35;
+            int op2 = 4;
+
+            Console.WriteLine($"O valor de {op1} & {op2} é {op1 & op2}");
+            Console.WriteLine($"O valor de {op1} | {op2} é {op1 | op2}");
+            Console.WriteLine($"O valor de {op1} ^ {op2} é {op1 ^ op2}");
+            Console.WriteLine($"O valor de {op1} ~ {op2} é {~op1} e {~op2}");
+            Console.WriteLine($"O valor de {op1} >> {op2} é {op1 >> op2}");
+            Console.WriteLine($"O valor de {op1} << {op2} é {op1 << op2}");
+
+            Console.ReadKey();
+        }
+    }
+}
