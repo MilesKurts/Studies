8251e92686777600f927c0bd024b9320e75de5ba
MilesKurts <milesheremau@gmail.com>
2024-04-28 10:36:05 -0300

    new

--- e_mail_2/Program.cs
+++ e_mail_2/Program.cs
@@ -11,10 +11,10 @@ namespace e_mail_2
     {
         static void Main(string[] args)
         {
-            var outlook = new Email("smtp.office365.com", "milesheremau@gmail.com", "Eunovo123");
+            var outlook = new Email("smtp.office365.com", "mauriciojuniorrosa8@gmail.com", "Eunovo123");
             outlook.SendEmail(new List<string>
             {
-                "mauriciojuniorrosa8@gmail.com"
+                "milesheremau@gmail.com"
             },
             subject: "test",
             body: "Segue anexo",
