4d0bb99822bcd02bdc2d34dbfaa2c40be5ab9de7
MilesKurts <milesheremau@gmail.com>
2024-05-03 00:35:47 -0300

    new

---
+++ fullstackweb_course/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fullstackweb_course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hi {name}, it's a pleasure knowing you!");
+            Console.ReadKey();
+            Console.WriteLine("Variable conversion - casting");
+            Console.ReadKey();
+            Console.WriteLine("Implicit casting byte->short->int->long->float->double");
+            Console.ReadKey();
+        }
+    }
+}
