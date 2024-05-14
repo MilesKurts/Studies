c960cd42270caf97707e5b9ea54617ea909ce5cc
MilesKurts <milesheremau@gmail.com>
2024-05-14 12:49:17 -0300

    new

---
+++ M02ex13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M02ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lotação = 10;
+            int total = 0;
+
+            int grupo = 0;
+            Console.Write("Qauntas pessoas chegaram?");
+            int.TryParse(Console.ReadLine(), out grupo);
+            total += grupo;
+
+            Console.WriteLine($"Chegaram um grupo de {grupo} pessoas, temos {total} pessoas no local");
+
+            Console.WriteLine($"lotou a balada? {total>=lotação}");
+            Console.ReadKey();
+
+        }
+    }
+}
