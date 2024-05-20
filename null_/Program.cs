0ea1da1f20ebb8467317ccf31c4cadd29c5f9479
MilesKurts <milesheremau@gmail.com>
2024-05-20 10:42:59 -0300

    new

---
+++ null_/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num = null;
+
+            int x = num ??900;
+
+            Console.WriteLine($"A variavel tem valor nulo? {num is null}");
+            Console.WriteLine($"A variavel tem valor? {num.HasValue}");
+            Console.WriteLine($"a VARIÁVEL X TEM VALOR? {x} ");
+            Console.ReadKey();
+        }
+    }
+}
