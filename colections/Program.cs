4bdae61c2faf192129ed99715c8d3edb30ea5493
MilesKurts <milesheremau@gmail.com>
2024-05-27 09:05:37 -0300

    new

---
+++ colections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace colections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            int[] numbers = new int[5];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+            Console.WriteLine(numbers[3]);
+            Console.ReadKey();
+        }
+    }
+}
