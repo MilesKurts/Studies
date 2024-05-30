094d1de64a1f47accf09f70f1ac7ad412aa8fe70
MilesKurts <milesheremau@gmail.com>
2024-05-30 09:39:58 -0300

    new

---
+++ while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 0;
+            while (valor<12)
+            {
+                Console.WriteLine($"O valor é {valor}");
+                valor++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
