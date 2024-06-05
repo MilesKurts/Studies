d1192778cb09c80c4f0c5962b84d1ad35b3361d6
MilesKurts <milesheremau@gmail.com>
2024-06-05 14:32:33 -0300

    new

--- Estaticas/Program.cs
+++ Estaticas/Program.cs
@@ -10,12 +10,18 @@ namespace Estaticas
     {
         static void Main(string[] args)
         {
-            Matematica.taxa = 10;
+            //Matematica.taxa = 10;
 
-            int valor1 = Matematica.Adicionar(200);
-            Console.WriteLine(valor1+"\n");
-            int valor2 = Matematica.Diminuir(200);
-            Console.WriteLine(valor2);
+            //            int valor1 = Matematica.Adicionar(200);
+            //          Console.WriteLine(valor1+"\n");
+            //        int valor2 = Matematica.Diminuir(200);
+            //      Console.WriteLine(valor2);//
+
+            Pessoa.maioridade = 21;
+            Pessoa p1 = new Pessoa();
+            p1.nome = "Gabriel";
+            p1.idade = Pessoa.CalcularIdade(1985);
+            Console.WriteLine(p1.nome +"\n"+ p1.idade);
             Console.ReadKey();
         }
     }
---
+++ Propriedades/Teste.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propriedades
+{
+    internal class Teste
+    {
+        private string _nome;
+        private string Sobrenome { get; } = "Artigas";
+
+        private int _idade;
+
+        public string Nome
+        {
+
+            get
+            {
+                return _nome;
+            } set
+            {
+                _nome = value;
+            } }
+
+        public int Idade
+        {
+
+            get
+            {
+                return _idade;
+            }
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Idade não pode ser inferior a 18 anos");
+                }
+                else
+                {
+                    _idade = value;
+                }
+            }
+        }
+        public void Apresentar()
+        {
+            if (_nome != "") 
+                Console.WriteLine($"seja bem vindo, {_nome}");
+        
+    }
+}
