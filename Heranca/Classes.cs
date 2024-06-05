c276d466a8d0f3dfcefa7a1adc3691ab4f2a3c4d
MilesKurts <milesheremau@gmail.com>
2024-06-05 16:15:41 -0300

    new

---
+++ Heranca/Classes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heranca
+{
+    internal class Humano
+    {
+        public virtual void Olhos()
+        {
+            Console.WriteLine("Humano.Olhos");
+        }
+        public virtual void Cabeloas()
+        {
+            Console.WriteLine("Humano.Cabelos");
+        }
+    }
+    class Pessoa : Humano
+    {
+        public override void Olhos()
+        {
+            Console.WriteLine("Humano.Olhos");
+        }
+        
+    }
+}
