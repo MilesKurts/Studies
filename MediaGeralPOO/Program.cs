aa912cc0f24ed064987a5a728ea464c4f2bf57a8
MilesKurts <milesheremau@gmail.com>
2024-06-07 09:54:27 -0300

    new

---
+++ MediaGeralPOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "### Media Geral dos alunos ###";
+
+            Console.WriteLine("Quantos alunos: ");
+            int nalunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Aluno[] alunos = new Aluno[nalunos];
+
+            for (int i = 0; i < alunos.Length; i++)
+            {
+                Console.Clear();
+                Console.Write($"Aluno # {i + 1} Nome..:");
+            }
+        }
+    }
+}
---
+++ Polimorfismo/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class CaixaEletronico : IConta
+    {
+        public string Usuario { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void Depositar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sacar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Saldo()
+        {
+            throw new NotImplementedException();
+        }
+        public void SolicitarEmprestimo()
+        {
+
+        }
+    }
+    interface  IConta
+    {
+        string Usuario { get; set; }
+        void Depositar();
+        void Sacar();
+        void Saldo();
+    }
+}
