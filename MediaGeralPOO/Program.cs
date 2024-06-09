9d8bb83fd7cf545d8b8806d342e6460410deaa25
MilesKurts <milesheremau@gmail.com>
2024-06-09 10:45:38 -0300

    new

--- MediaGeralPOO/Program.cs
+++ MediaGeralPOO/Program.cs
@@ -19,11 +19,34 @@ namespace MediaGeralPOO
 
             Aluno[] alunos = new Aluno[nalunos];
 
-            for (int i = 0; i < alunos.Length; i++)
+            for (int i = 0; i < alunos.Length; i++) 
             {
                 Console.Clear();
                 Console.Write($"Aluno # {i + 1} Nome..:");
+                string nome = Console.ReadLine();
+
+                Console.WriteLine($"Aluno #{i+1} provas: ");
+                int provas = int.Parse(Console.ReadLine());
+
+                alunos[i] = new Aluno(nome, provas);
+
+                Console.WriteLine($"Insira as notas do aluno {nome}");
+                alunos[i].InserirNotas();
             }
+            Console.Clear();
+
+            double mediaGeral = 0;
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine($"Aluno {aluno.Nome}");
+                Console.WriteLine($"Media {aluno.Media}");
+                Console.WriteLine();
+                mediaGeral += aluno.Media;
+            }
+            double resultadoFinal = mediaGeral / alunos.Length;
+            Console.WriteLine($"Média geral dos alunos: {resultadoFinal}");
+            Console.ReadKey();
         }
     }
 }
