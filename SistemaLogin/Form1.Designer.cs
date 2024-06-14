e8cc1378c02bba69d3cf14853df5cd148f691143
MilesKurts <milesheremau@gmail.com>
2024-06-14 09:12:13 -0300

    new

---
+++ SistemaLogin/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SistemaLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelBoas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelBoas
+            // 
+            this.labelBoas.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBoas.Location = new System.Drawing.Point(176, 134);
+            this.labelBoas.Name = "labelBoas";
+            this.labelBoas.Size = new System.Drawing.Size(448, 182);
+            this.labelBoas.TabIndex = 0;
+            this.labelBoas.Text = "Tela Inicial";
+            this.labelBoas.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelBoas);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelBoas;
+    }
+}
+
