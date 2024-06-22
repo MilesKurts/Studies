0d931162a661215c257bfd13d810d456ffc6d11c
MilesKurts <milesheremau@gmail.com>
2024-06-22 11:50:37 -0300

    new

---
+++ Formulario1/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Formulario1
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnSegunda = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(35, 29);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(680, 209);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Principal";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnSegunda
+            // 
+            this.btnSegunda.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSegunda.Location = new System.Drawing.Point(42, 286);
+            this.btnSegunda.Name = "btnSegunda";
+            this.btnSegunda.Size = new System.Drawing.Size(169, 67);
+            this.btnSegunda.TabIndex = 1;
+            this.btnSegunda.Text = "Segunda Form";
+            this.btnSegunda.UseVisualStyleBackColor = true;
+            this.btnSegunda.Click += new System.EventHandler(this.btnSegunda_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSegunda);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "FormMain";
+            this.Text = "Formulario Principal";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnSegunda;
+    }
+}
+
