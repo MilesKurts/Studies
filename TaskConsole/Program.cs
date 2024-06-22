1e44f0ec98e0ee23f52cc5d1e06fe529f590c28c
MilesKurts <milesheremau@gmail.com>
2024-06-21 23:47:10 -0300

    new

---
+++ TaskConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            //Task t1 = new Task(Tarefa);
+            //t1.Start();
+
+            //            Task.Run(Tarefa);
+
+            //Task.Factory.StartNew(Tarefa);
+
+            //Task.Run(() => {
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine("Tarefa anônima.");
+            //    }
+            //});
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Principal.");
+            //}
+            #endregion
+            Task[] tasks = 
+            {
+                Task.Factory.StartNew(() => 
+                {
+                    Console.WriteLine("Tarefa #1");
+                }),
+                Task.Factory.StartNew(() => 
+                {
+                    Console.WriteLine("Tarefa #2");
+                }),
+                Task.Factory.StartNew(() => 
+                {
+                    Console.WriteLine("Tarefa #3");
+                })
+            };
+
+            Task.WaitAll(tasks);
+
+            Console.WriteLine("Principal");
+            Console.ReadKey();
+        }
+        static private void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa do task.");
+            }
+
+        }
+    }
+}
