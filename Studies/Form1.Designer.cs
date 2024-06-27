734613210114fb02826308872cf85cc8ec0b90d8
MilesKurts <milesheremau@gmail.com>
2024-06-27 00:13:27 -0300

    new

--- Studies/Form1.Designer.cs
+++ Studies/Form1.Designer.cs
@@ -29,10 +29,11 @@
         private void InitializeComponent()
         {
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.lista = new System.Windows.Forms.ListView();
             this.colNome = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colTel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.colEmail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // dateTimePicker1
@@ -42,19 +43,23 @@
             this.dateTimePicker1.Size = new System.Drawing.Size(313, 20);
             this.dateTimePicker1.TabIndex = 0;
             // 
-            // listView1
+            // lista
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lista.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.colNome,
             this.colTel,
             this.colEmail});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(72, 101);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(422, 274);
-            this.listView1.TabIndex = 1;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.lista.FullRowSelect = true;
+            this.lista.HideSelection = false;
+            this.lista.LabelEdit = true;
+            this.lista.LabelWrap = false;
+            this.lista.Location = new System.Drawing.Point(72, 101);
+            this.lista.MultiSelect = false;
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(422, 274);
+            this.lista.TabIndex = 1;
+            this.lista.UseCompatibleStateImageBehavior = false;
+            this.lista.View = System.Windows.Forms.View.Details;
             // 
             // colNome
             // 
@@ -71,12 +76,23 @@
             this.colEmail.Text = "Email";
             this.colEmail.Width = 100;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lista);
             this.Controls.Add(this.dateTimePicker1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -87,10 +103,11 @@
         #endregion
 
         public System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.ListView listView1;
         private System.Windows.Forms.ColumnHeader colNome;
         private System.Windows.Forms.ColumnHeader colTel;
         private System.Windows.Forms.ColumnHeader colEmail;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lista;
     }
 }
 
--- Studies/Form1.cs
+++ Studies/Form1.cs
@@ -15,6 +15,30 @@ namespace Studies
         public Form1()
         {
             InitializeComponent();
+
+            ListViewItem item1 = new ListViewItem("Miles");
+            item1.SubItems.Add("199999999");
+            item1.SubItems.Add("hjsjahsh@gusa.com");
+
+            ListViewItem item2 = new ListViewItem("Grazi");
+            item2.SubItems.Add("199999999");
+            item2.SubItems.Add("hjsjahsh@gusa.com");
+
+            ListViewItem item3 = new ListViewItem("Yuno");
+            item3.SubItems.Add("199999999");
+            item3.SubItems.Add("hjsjahsh@gusa.com");
+
+            ListViewItem item4 = new ListViewItem(new string[] {"Paula", "19999999999", "kljhdsjahkldjsa@hofjoads.com"});
+
+            lista.Items.Add(item1);
+            lista.Items.Add(item2);
+            lista.Items.Add(item3);
+            lista.Items.Add(item4);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lista.Items.Remove(lista.SelectedItems[0]);
         }
     }
 }
