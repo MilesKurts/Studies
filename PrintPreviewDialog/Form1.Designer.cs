c5e645bb9d10d9563aecd054c082c8712a40206f
MilesKurts <milesheremau@gmail.com>
2024-07-03 12:35:42 -0300

    new

--- PrintPreviewDialog/Form1.Designer.cs
+++ PrintPreviewDialog/Form1.Designer.cs
@@ -33,6 +33,9 @@
             this.txtPrint = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.printDoc = new System.Drawing.Printing.PrintDocument();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.SuspendLayout();
             // 
             // printPreview
@@ -49,13 +52,13 @@
             // 
             this.txtPrint.Location = new System.Drawing.Point(41, 34);
             this.txtPrint.Name = "txtPrint";
-            this.txtPrint.Size = new System.Drawing.Size(580, 295);
+            this.txtPrint.Size = new System.Drawing.Size(437, 281);
             this.txtPrint.TabIndex = 0;
             this.txtPrint.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(240, 342);
+            this.button1.Location = new System.Drawing.Point(118, 348);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(195, 61);
             this.button1.TabIndex = 1;
@@ -67,16 +70,38 @@
             // 
             this.printDoc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDoc_PrintPage);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(581, 191);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Selecione a cor";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(585, 363);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cor";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtPrint);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +111,9 @@
         private System.Windows.Forms.RichTextBox txtPrint;
         private System.Windows.Forms.Button button1;
         private System.Drawing.Printing.PrintDocument printDoc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
     }
 }
 
