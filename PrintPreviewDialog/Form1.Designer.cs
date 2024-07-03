5cdcff44d88c17a4bc43e49d28a62a754aba1ffb
MilesKurts <milesheremau@gmail.com>
2024-07-03 10:48:42 -0300

    new

---
+++ PrintPreviewDialog/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace PrintPreviewDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.printPreview = new System.Windows.Forms.PrintPreviewDialog();
+            this.txtPrint = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printDoc = new System.Drawing.Printing.PrintDocument();
+            this.SuspendLayout();
+            // 
+            // printPreview
+            // 
+            this.printPreview.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreview.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreview.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreview.Enabled = true;
+            this.printPreview.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreview.Icon")));
+            this.printPreview.Name = "printPreview";
+            this.printPreview.Visible = false;
+            // 
+            // txtPrint
+            // 
+            this.txtPrint.Location = new System.Drawing.Point(41, 34);
+            this.txtPrint.Name = "txtPrint";
+            this.txtPrint.Size = new System.Drawing.Size(580, 295);
+            this.txtPrint.TabIndex = 0;
+            this.txtPrint.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Imprimir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printDoc
+            // 
+            this.printDoc.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDoc_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPrint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewDialog printPreview;
+        private System.Windows.Forms.RichTextBox txtPrint;
+        private System.Windows.Forms.Button button1;
+        private System.Drawing.Printing.PrintDocument printDoc;
+    }
+}
+
