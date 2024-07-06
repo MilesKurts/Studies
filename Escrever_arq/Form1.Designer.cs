1cb27eb4be84728244e371d34373048eef7fda4e
MilesKurts <milesheremau@gmail.com>
2024-07-06 09:50:15 -0300

    new

---
+++ Escrever_arq/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Escrever_arq
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConteudo = new System.Windows.Forms.TextBox();
+            this.btnEscreverTxt = new System.Windows.Forms.Button();
+            this.leiaTxt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtConteudo
+            // 
+            this.txtConteudo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConteudo.Location = new System.Drawing.Point(57, 21);
+            this.txtConteudo.Multiline = true;
+            this.txtConteudo.Name = "txtConteudo";
+            this.txtConteudo.Size = new System.Drawing.Size(629, 259);
+            this.txtConteudo.TabIndex = 0;
+            // 
+            // btnEscreverTxt
+            // 
+            this.btnEscreverTxt.Location = new System.Drawing.Point(203, 312);
+            this.btnEscreverTxt.Name = "btnEscreverTxt";
+            this.btnEscreverTxt.Size = new System.Drawing.Size(154, 78);
+            this.btnEscreverTxt.TabIndex = 1;
+            this.btnEscreverTxt.Text = "Escrever txt";
+            this.btnEscreverTxt.UseVisualStyleBackColor = true;
+            this.btnEscreverTxt.Click += new System.EventHandler(this.btnEscreverTxt_Click);
+            // 
+            // leiaTxt
+            // 
+            this.leiaTxt.Location = new System.Drawing.Point(416, 312);
+            this.leiaTxt.Name = "leiaTxt";
+            this.leiaTxt.Size = new System.Drawing.Size(162, 86);
+            this.leiaTxt.TabIndex = 2;
+            this.leiaTxt.Text = "Leia";
+            this.leiaTxt.UseVisualStyleBackColor = true;
+            this.leiaTxt.Click += new System.EventHandler(this.leiaTxt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.leiaTxt);
+            this.Controls.Add(this.btnEscreverTxt);
+            this.Controls.Add(this.txtConteudo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConteudo;
+        private System.Windows.Forms.Button btnEscreverTxt;
+        private System.Windows.Forms.Button leiaTxt;
+    }
+}
+
---
+++ Escrever_arq/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Escrever_arq
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEscreverTxt_Click(object sender, EventArgs e)
+        {
+            string path = @"C:\Users\Mile kurtz\source\repos\ambM01\Escrever_arq\text\novo.txt";
+            StreamWriter writer = new StreamWriter(path, true, Encoding.Default);
+
+            string linha = txtConteudo.Text;
+            writer.WriteLine(linha);
+
+            writer.Flush();
+            writer.Dispose();
+            writer.Close();
+        }
+
+        private void leiaTxt_Click(object sender, EventArgs e)
+        {
+            txtConteudo.Clear();
+            string path = @"C:\Users\Mile kurtz\source\repos\ambM01\Escrever_arq\text\novo.txt";
+
+            StreamReader reader = new StreamReader(path, Encoding.Default);
+
+            string txt = reader.ReadToEnd();
+
+            txtConteudo.Text = txt;
+        }
+    }
+}
