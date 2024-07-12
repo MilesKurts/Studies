948b17a320e0e9a7e7b7cd705ff717ada9e29b4c
MilesKurts <milesheremau@gmail.com>
2024-07-12 13:33:13 -0300

    new

---
+++ GDI+/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GDI_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDesenhar_Click(object sender, EventArgs e)
+        {
+            Bitmap folha = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            Graphics desenhador = Graphics.FromImage(folha);
+
+            desenhador.Clear(Color.Red);
+
+            pictureBox1.BackgroundImage = folha;
+        }
+    }
+}
